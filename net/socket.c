e8126c62f42119e38bc6aee1d13bec3703058a03
ShivamKumarJha <jha.shivam3@gmail.com>
2020-04-25 22:27:02 +0530

    Add net/ modifications
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- net/socket.c
+++ net/socket.c
@@ -437,6 +437,7 @@ struct file *sock_alloc_file(struct socket *sock, int flags, const char *dname)
 	sock->file = file;
 	file->f_flags = O_RDWR | (flags & O_NONBLOCK);
 	file->private_data = sock;
+
 	return file;
 }
 EXPORT_SYMBOL(sock_alloc_file);
@@ -2097,8 +2098,9 @@ static int ___sys_sendmsg(struct socket *sock, struct user_msghdr __user *msg,
 	}
 
 out_freectl:
-	if (ctl_buf != ctl)
+	if (ctl_buf != ctl) 
 		sock_kfree_s(sock->sk, ctl_buf, ctl_len);
+
 out_freeiov:
 	kfree(iov);
 	return err;
